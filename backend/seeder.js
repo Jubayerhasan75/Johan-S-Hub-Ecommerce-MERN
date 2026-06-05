d7712e240a6884b3435daedd4d5fbb8a37b8bd97
Jubayer Hasan <125861868+Jubayerhasan75@users.noreply.github.com>
2026-06-05 12:49:30 +0600

    Fix import statement for products data

--- backend/seeder.js
+++ backend/seeder.js
@@ -4,7 +4,7 @@ import User from './models/User.js';
 import Order from './models/Order.js';
 import Product from './models/Product.js';
 import adminUser from './data/adminUser.js';
-import products from './data/products.js'; // ⛔️ Apnar Product List
+import products from './data/products.js'; //
 
 dotenv.config();
 
@@ -25,7 +25,7 @@ const importData = async () => {
     await Product.deleteMany();
     await User.deleteMany();
 
-    // 2. Notun Admin ebong Product dhukano
+    
     await User.insertMany(adminUser);
     await Product.insertMany(products); 
 
